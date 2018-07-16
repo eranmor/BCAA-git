5d7220e9060767ef99a961ea39a34e6f55ea71c2
Eran mor <eranmr@yahoo.com>
2018-07-16 15:59:08 -0700

    .

--- SetupOracleClient.ps1
+++ SetupOracleClient.ps1
@@ -2,12 +2,21 @@
 # Author: Eran Mor eran.mor@bcaa.com
 
 # Variables
-$oracleClientSourcePath = '\\bcaa.bc.ca\go\Support\Sw_apps\Oracle\12c\client'
-$oracleClientSetupExecutable = 'D:\OracleClient12\setup.exe'
+$oracleClientSourcePath = "\\bcaa.bc.ca\go\Support\Sw_apps\Oracle\12c\client"
+$oracleClientSetupExecutable = "d:\OracleClient12\setup.exe"
 
-# Copy and install oracle client 12c
+Write-Host "Copy and install oracle client 12c"
 
-Copy-Item -from  $oracleClientSourcePath -to 'd:\' -wait
-Rename-Item -Path 'd:\client' -NewName "OracleClient12"
-& $oracleClientSetupExecutable -wait
-Copy-Item -from 'D:\Oracle\product\12.1.0\client_1\jdbc\lib' -to 'D:\jboss\jboss-eap-6.4.0\jboss-eap-6.4\standalone\deployments\'
+Copy-Item $oracleClientSourcePath -Destination "d:\" -Recurse -Verbose -Force
+
+Rename-Item -Path "d:\client" -NewName "OracleClient12" -Force
+
+Write-Host "Install OracleClient12"
+
+Start-Process $oracleClientSetupExecutable -NoNewWindow -Wait
+
+Copy-Item "d:\Oracle\product\12.1.0\client_1\jdbc\lib" -Destination "d:\jboss\jboss-eap-6.4.0\jboss-eap-6.4\standalone\deployments\" -Recurse -Verbose -Force
+
+Write-Host "Restarting JBossEAP6 service"
+
+Restart-Service -Name JBossEAP6
