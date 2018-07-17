19a1be490ecdfa101766ecf69b6b7214ba1e2898
Eran mor <eranmr@yahoo.com>
2018-07-16 17:12:38 -0700

    .

--- CawaSetup.ps1
+++ CawaSetup.ps1
@@ -1,10 +1,15 @@
 # Cawa clinet setup and FW rule
 
-.\setup.exe -f "win_installer.properties"
+Write-Host 'Installing CAWA Agent'
+
+.\setup.exe -f "win_installer.properties" -NoNewWindow -Wait
+
+Start-Sleep -s 60
 
 Write-Host "Creating FW rule"
 netsh advfirewall firewall add rule name="Allow CAWA Connections" dir=in action=allow protocol=TCP localport=7520 remoteip=192.197.3.48,192.197.3.49
 
-Write-hos "Setting CA Workload Automation Agent to start automatically"
-Set-Service -Name "CA Workload Automation Agent" -StartupType Automatic
-Start-Service -Name "CA Workload Automation Agent"
+Write-host "Setting CA Workload Automation Agent to start automatically"
+
+Set-Service -Name 'CA Workload Automation Agent' -StartupType Automatic
+Start-Service -Name 'CA Workload Automation Agent'
