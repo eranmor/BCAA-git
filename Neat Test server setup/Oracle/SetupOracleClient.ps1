03b511e1e5a44da827e1c848e5c7ea6e75a08bfa
Eran mor <eranmr@yahoo.com>
2018-07-19 11:44:40 -0700

    .

--- Neat Test server setup/Oracle/SetupOracleClient.ps1
+++ Neat Test server setup/Oracle/SetupOracleClient.ps1
@@ -3,7 +3,7 @@
 
 # Variables
 $oracleClientSourcePath = "\\bcaa.bc.ca\go\Support\Sw_apps\Oracle\12c\client"
-$oracleClientSetupExecutable = "d:\OracleClient12\setup.exe"
+# $oracleClientSetupExecutable = "'d:\OracleClient12\setup.exe' -silent 'variable=setting' -nowelcome -nowait -noconfig -responseFilק 'client_install.rsp'"
 
 Write-Host "Copy and install oracle client 12c"
 
@@ -13,10 +13,14 @@ Rename-Item -Path "d:\client" -NewName "OracleClient12" -Force
 
 Write-Host "Install OracleClient12"
 
-Start-Process $oracleClientSetupExecutable -NoNewWindow -Wait
+New-Item -ItemType directory -Path "d:\Oracle"
+
+cmd.exe /c "d:\OracleClient12\setup.exe -silent -nowelcome  -responseFile 'd:\tmp\Neat Test server setup\Oracle\client_install.rsp'"
+
+Start-Sleep -s 30
 
 Copy-Item "d:\Oracle\product\12.1.0\client_1\jdbc\lib" -Destination "d:\jboss\jboss-eap-6.4.0\jboss-eap-6.4\standalone\deployments\" -Recurse -Verbose -Force
 
 Write-Host "Restarting JBossEAP6 service"
 
-Restart-Service -Name JBossEAP6
+# Restart-Service -Name JBossEAP6
