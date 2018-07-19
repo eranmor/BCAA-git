608ab9222510dd41b86b26234d97e01006456520
Eran mor <eranmr@yahoo.com>
2018-07-19 14:23:05 -0700

    CAWA setup as a service - tested ok

--- Neat Test server setup/CAWA/CawaSetup.ps1
+++ Neat Test server setup/CAWA/CawaSetup.ps1
@@ -1,9 +1,10 @@
-# Cawa clinet setup and FW rule
+# Cawa client setup and FW rule
 Write-Host 'Installing CAWA Agent'
 
-& cmd.exe /c 'setup.exe -f "win_installer.properties"' -NoNewWindow -nowait
+& cmd.exe /c setup.exe -f "win_installer.properties"
 
 Start-Sleep -s 30
+
 
 Write-Host "Creating FW rule"
 netsh advfirewall firewall add rule name="Allow CAWA Connections" dir=in action=allow protocol=TCP localport=7520 remoteip=192.197.3.48,192.197.3.49
@@ -11,4 +12,5 @@ netsh advfirewall firewall add rule name="Allow CAWA Connections" dir=in action=
 Write-host "Setting CA Workload Automation Agent to start automatically"
 
 Set-Service -Name 'CA Workload Automation Agent' -StartupType Automatic
-Start-Service -Name 'CA Workload Automation Agent'
+
+# Start-Service -Name 'CA Workload Automation Agent'
