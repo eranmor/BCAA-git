e73b63895c52e4a6123f2e3c44c055ff17caf3a8
Eran mor <eranmr@yahoo.com>
2018-07-19 14:02:19 -0700

    Java setup

---
+++ Neat Test server setup/JAVA/JavaSetup.bat
@@ -0,0 +1,4 @@
+echo
+Installing java
+@echo off
+\\nova\Sierra\freeware\JDK\java8\jdk-8u172-windows-x64.exe /s ADDLOCAL="ToolsFeature,SourceFeature,PublicjreFeature" INSTALLDIR=D:\Java\jdk1.8.0_172 /INSTALLDIRPUBJRE=D:\Java\jre1.8.0_172
