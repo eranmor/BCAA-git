c3e313814cfd4060339c028f5cf4eac069f45775
Eran mor <eranmr@yahoo.com>
2018-07-25 10:18:01 -0700

    ip variables

--- Neat Test server setup/CAWA/CawaSetup.ps1
+++ Neat Test server setup/CAWA/CawaSetup.ps1
@@ -1,4 +1,7 @@
 # Cawa client setup and FW rule
+
+$CawaServerIp1=192.197.3.48
+$CawaServerIp2=192.197.3.49
 Write-Host 'Installing CAWA Agent'
 
 & cmd.exe /c setup.exe -f "win_installer.properties"
@@ -7,7 +10,7 @@ Start-Sleep -s 30
 
 
 Write-Host "Creating FW rule"
-netsh advfirewall firewall add rule name="Allow CAWA Connections" dir=in action=allow protocol=TCP localport=7520 remoteip=192.197.3.48,192.197.3.49
+netsh advfirewall firewall add rule name="Allow CAWA Connections" dir=in action=allow protocol=TCP localport=7520 remoteip=$CawaServerIp1,$CawaServerIp2
 
 Write-host "Setting CA Workload Automation Agent to start automatically"
 
